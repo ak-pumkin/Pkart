e9c4c43e8e1818235341dc3e7ef81be528257811
ak-pumkin <saidevrajan18@gmail.com>
2025-08-09 14:41:55 +0530

    Delete backend directory

--- backend/models/Product.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const productSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    description: String,
-    price: { type: Number, required: true },
-    category: String
-}, { timestamps: true });
-
-module.exports = mongoose.model('Product', productSchema);
